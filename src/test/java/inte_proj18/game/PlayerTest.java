f810bfd6b91205a946bbce20165d15b0d4a5b4d2
daan3440 <daan3440@su.se>
2018-10-15 13:23:40 +0200

    Lite nhytt Signed-off-by: daan3440 <daan3440@su.se>

--- src/test/java/inte_proj18/game/PlayerTest.java
+++ src/test/java/inte_proj18/game/PlayerTest.java
@@ -1,15 +1,37 @@
 package inte_proj18.game;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
-	
-@Test
-public void createPlayerTest(){
-	
-}
+	Player player;
 
+	@BeforeEach
+	void createPlayer() {
+		player = new Player("stina");
+	}
 
+	@Test
+	public void createPlayerCheckNullTest() {
+		assertNotNull(player);
+	}
+
+	@Test
+	public void playerNameTest() {
+		assertEquals(player.getName(), "stina");
+
+	}
+
+	@Test
+	public void enteredGameMapTest() {
+		Position pos = Position (32, 32);
+		GameMap gamemap = GameMap(64, 64);
+		player.enterMap(pos, gamemap);
+		
+		
+		
+	}
 
 }
