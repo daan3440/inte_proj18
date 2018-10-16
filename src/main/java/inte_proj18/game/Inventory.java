5efe65309829aafbd82fba07786920c043761a83
AntonShad <anton.harneby@gmail.com>
2018-10-16 13:10:00 +0200

    Iventory start, Item start
    Stubs and tests have been started for both inventory and items.
    
    Signed Anton Harneby

--- src/main/java/inte_proj18/game/Inventory.java
+++ src/main/java/inte_proj18/game/Inventory.java
@@ -1,5 +1,11 @@
 package inte_proj18.game;
 
 public class Inventory {
+	
+	Item[] items;
+	
+	public Inventory() {
+		items = new Item[10];
+	}
 
 }
---
+++ src/main/java/inte_proj18/game/Item.java
@@ -0,0 +1,19 @@
+package inte_proj18.game;
+
+public class Item {
+	
+	private String name;
+	
+	public Item() {
+		
+	}
+
+	public Item(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
