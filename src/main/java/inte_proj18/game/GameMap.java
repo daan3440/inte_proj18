5acdd1f3f3a102140ecf7f24ba5759c1f99023b9
OlofP <43401107+OlofP@users.noreply.github.com>
2018-10-16 11:52:06 +0200

    påbörjat HashMap på gamemap

--- src/main/java/inte_proj18/game/GameMap.java
+++ src/main/java/inte_proj18/game/GameMap.java
@@ -1,11 +1,18 @@
 package inte_proj18.game;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class GameMap {
 	private int width;
 	private int height;
+	private Map<Position, Object> mapObjects;
+	
 	public GameMap(int width, int height) {
 		this.width=width;
 		this.height=height;
+		mapObjects = new HashMap();
+//		mapObjects.put(key, value)
 	}
 	
 	public int getWidth(){
@@ -13,6 +20,10 @@ public class GameMap {
 	}
 	public int getHeight(){
 		return height;
+	}
+	
+	public Map getGameMapObjects() {
+		return mapObjects;
 	}
 
 }
--- src/main/java/inte_proj18/game/Position.java
+++ src/main/java/inte_proj18/game/Position.java
@@ -15,7 +15,6 @@ public class Position {
 	}
 	
 	public int getY() {
-	
 		return y;
 	}
 	
