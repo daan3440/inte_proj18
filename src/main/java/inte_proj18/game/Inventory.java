f636560bb9ec9f47b21d129b1fe83b3e87ee57d8
AntonShad <anton.harneby@gmail.com>
2018-10-19 15:05:56 +0200

    Added Inventory listing and Start of ItemReader

--- src/main/java/inte_proj18/game/Inventory.java
+++ src/main/java/inte_proj18/game/Inventory.java
@@ -43,6 +43,14 @@ public class Inventory {
 		}
 		return null;
 	}
+	
+	public String listItems() {
+		String output = "";
+		for (int i = 0; i < ArraySize; i++) {
+			output = output + "\n" + items[i];
+		}
+		return output;
+	}
 
 //	public int getEmptyPlace() {
 //		int emptyPlace = 0;
