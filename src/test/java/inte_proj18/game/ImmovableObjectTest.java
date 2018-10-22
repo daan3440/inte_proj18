b8b90231d42794c2aa2ae8c97ff524aec9b97cb6
daan3440 <daan3440@su.se>
2018-10-22 11:49:41 +0200

    Merge conflicts Fix
    Signed-off-by: daan3440 <daan3440@su.se>

--- src/test/java/inte_proj18/game/ImmovableObjectTest.java
+++ src/test/java/inte_proj18/game/ImmovableObjectTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ImmovableObjectTest {
-	static final String VALIDNAME = "#";
+	static final String VALIDNAME = "ImmovableO";
 	ImmovableObject io;
 	@BeforeEach
 	void setUp() {
